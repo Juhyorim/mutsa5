6d76c8c91362dd2ca247c61a3392317b56c6ca16
Juhyorim <joohr1234@gmail.com>
2023-05-21 22:41:57 +0900

    initial commit

---
+++ src/main/java/dev/lime/mutsa5/Mutsa5Application.java
@@ -0,0 +1,13 @@
+package dev.lime.mutsa5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mutsa5Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Mutsa5Application.class, args);
+  }
+
+}
